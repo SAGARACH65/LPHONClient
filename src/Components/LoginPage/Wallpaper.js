84974d0ce81b20de5203ebcc7e0691d3a7fe8ec1
Sagar Acharya <sagarach65@gmail.com>
2018-05-07 18:19:08 +0545

    Login page added

---
+++ src/Components/LoginPage/Wallpaper.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Dimensions from 'Dimensions';
+import {StyleSheet, ImageBackground,StatusBar} from 'react-native';
+
+import bgSrc from './wallpaper.png';
+
+export default class Wallpaper extends Component {
+    render() {
+        return (
+            <ImageBackground style={styles.picture} source={bgSrc}>
+                {this.props.children}
+                <StatusBar hidden={true} />
+            </ImageBackground>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    picture: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover',
+    },
+});
